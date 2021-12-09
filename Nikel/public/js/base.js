ecd06e4020864da7e4d8677834fc123e166a64a8
Felipe André <felipeandre8@outlook.com>
2021-12-08 21:48:57 -0300

    alteraçoes de código

---
+++ Nikel/public/js/base.js
@@ -0,0 +1,25 @@
+ let nomes = ["Marcelo Eltz", "Felipe André", "Joao da Silva"];
+
+ let pessoas = [{
+         nome: "Felipe André",
+         idade: "26",
+         trabalho: "programador",
+     },
+     {
+         nome: "João da silva",
+         idade: "26",
+         trabalho: "UI UX",
+     }
+ ];
+
+
+
+ function adicionarPesso(pessoa) {
+     pessoas.push(pessoa);
+ }
+
+
+
+ console.log(pessoas);
+
+ //imprimirPessoa(pessoaDefault);
